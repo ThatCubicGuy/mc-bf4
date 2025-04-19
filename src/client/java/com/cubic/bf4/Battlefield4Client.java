3968aa496d190d053526ba0c01108eb8ba4ef6e0
ThatCubicGuy <vlasinalexcatalin@gmail.com>
2025-04-20 01:39:46 +0300

    Add project files

---
+++ src/client/java/com/cubic/bf4/Battlefield4Client.java
@@ -0,0 +1,11 @@
+package com.cubic.bf4;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Battlefield4Client implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        // help
+    }
+}
---
+++ src/main/java/com/cubic/bf4/Battlefield4.java
@@ -0,0 +1,11 @@
+package com.cubic.bf4;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Battlefield4 implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        // help
+    }
+}
